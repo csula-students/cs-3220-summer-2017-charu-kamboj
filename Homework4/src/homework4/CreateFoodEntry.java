d2fcffdbc63ab41661995ba2af95c3823aa46f55
charu-kamboj <charu.kkamboj@gmail.com>
2017-08-10 14:02:22 -0700

    Initial Commit started homework 4

---
+++ Homework4/src/homework4/CreateFoodEntry.java
@@ -0,0 +1,42 @@
+package homework4;
+
+public class CreateFoodEntry {
+
+	final int id; 
+	final String foodName;
+	final String imgurl;
+	final String description;
+	final double price;
+	
+	public CreateFoodEntry(int id, String item, String image, String description, double price) {
+
+		this.id = id;
+		this.foodName = item;
+		this.description = description;
+		this.imgurl = image;
+		this.price = price;
+		
+	}
+	
+	public int getId(){
+		return id; 
+	}
+	
+	public String getName(){
+		return foodName;
+	}
+	
+	public String getImage(){
+		return imgurl;
+	}
+	
+	public String getDesc(){
+		return description;
+	}
+	public double getPrice(){
+		return price;
+		
+	}
+	
+	
+}
---
+++ Homework4/src/homework4/EditFoodServlet.java
@@ -0,0 +1,56 @@
+
+package homework4;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/EditFoodServlet") 
+public class EditFoodServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		System.out.println(id);
+
+		CreateFoodEntryDAO dao = new CreateFoodEntryDAO();
+		System.out.println(dao.get(id));
+		CreateFoodEntry itemToEdit = dao.get(id).get();
+
+		request.setAttribute("itemToEdit", itemToEdit);
+
+		request.getRequestDispatcher("editfood.jsp")
+            .forward(request, response);
+	}
+
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		CreateFoodEntry Food_update = new CreateFoodEntry(
+			Integer.parseInt(request.getParameter("id")),
+			request.getParameter("name"),
+			request.getParameter("img"),
+			request.getParameter("desc"),
+			Double.parseDouble(request.getParameter("price"))
+			);
+
+		CreateFoodEntryDAO dao = new CreateFoodEntryDAO();
+		dao.update(Food_update);
+
+		response.sendRedirect(request.getContextPath()+"/Inventory");
+	}
+}
